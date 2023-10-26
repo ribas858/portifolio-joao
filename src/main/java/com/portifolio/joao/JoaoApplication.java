718f6a2b3ba2335a53ae9801036d2d63e4f9b367
ribas858 <ribas858@gmail.com>
2023-10-26 11:45:46 -0300

    primeiro commit

---
+++ src/main/java/com/portifolio/joao/JoaoApplication.java
@@ -0,0 +1,13 @@
+package com.portifolio.joao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaoApplication.class, args);
+	}
+
+}
